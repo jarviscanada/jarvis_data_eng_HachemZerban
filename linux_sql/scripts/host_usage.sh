4b35264464ec597284454bce8f61a48ffbb1f994
TechzPro <95704514+TechzPro@users.noreply.github.com>
2021-12-29 10:57:23 -0500

    Update host_usage.sh

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -13,8 +13,9 @@ if [ $# -ne 5 ]; then
     echo ' Command missing ' 
      exit 1 
     fi
-
+#retrieve host data and store it in variables
 hostname=$(hostname -f | xargs)
+# retrieving the 3rd line row 18 and 19 
 timestamp=$(vmstat -t | awk 'FNR == 3 {print $18" "$19}'| xargs)
 vmstat_mb=$(vmstat --unit M)
 #usage
@@ -24,10 +25,10 @@ cpu_kernel=$(echo "$vmstat_mb" | awk 'FNR == 3 {print $14}' | xargs)
 disk_io=$(vmstat -d | awk 'FNR == 3 {print $10}' | xargs)
 
 disk_available=$(df -BM | awk 'FNR == 6 {print $4}' | xargs)
-
+#query to get the host id by host name for host_info table
 host_id="(SELECT id FROM host_info WHERE hostname='$hostname')"
 
-
+#storing all retrieved data into the database
 insert_stmt="INSERT INTO host_usage(timestamp,host_id,memory_free,cpu_idle,cpu_kernel,disk_io,disk_available) 
 VALUES ('$timestamp',$host_id,'$memory_free','$cpu_idle','$cpu_kernel','$disk_io','$disk_available');"
 
@@ -35,7 +36,7 @@ VALUES ('$timestamp',$host_id,'$memory_free','$cpu_idle','$cpu_kernel','$disk_io
 
 #set up env var for pql cmd
 export PGPASSWORD=$psql_password 
-#Insert date into a database
+# executing the insert statement 
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
 exit $?
 
