ef596ecd4563587177c5cdc5996044d0b3554533
Hachem Zerban <tech.pro.hz@gmail.com>
2022-01-26 18:48:41 +0000

    practice

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/TwoSums.java
@@ -0,0 +1,38 @@
+package ca.jrvs.practice.codingChallenge;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSums {
+
+  public static void main(String[] args) {
+    int []arr = {2,11,7};
+    int [] res = checkSum(arr,9);
+    System.out.println(Arrays.toString(res));
+
+  }
+  public static  int [] checkSum(int[] arr , int target) {
+  int index =0 ;
+  int [] result = new int[2];
+    HashMap<Integer,Integer> map1= new HashMap<>();
+    for (int element :arr) {
+      if (!map1.containsKey(target-element)) {
+        map1.put(element,index);
+        index++ ;
+
+      }
+      else {
+        result[0]=index ;
+        result[1]= element;
+
+
+      }
+
+    }
+
+    return  result;
+
+  }
+
+
+}
---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/evenOdd.java
@@ -0,0 +1,15 @@
+package ca.jrvs.practice.codingChallenge;
+
+public class evenOdd {
+
+  public static void main(String[] args) {
+    System.out.println(checkoddEven(9));
+
+  }
+  public static String checkoddEven(int n) {
+
+    return  n%2 == 0 ? "odd":"even";
+
+
+  }
+}
